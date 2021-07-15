387ad584279e686f2b66e5310f1b037b4d76f3b3
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-07-15 22:50:52 +0700

    up

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import VueBarcodeScanner from 'vue-barcode-scanner'
 Vue.config.productionTip = false
+Vue.use(VueBarcodeScanner)
 
 new Vue({
   router,
