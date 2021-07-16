3a82a5283c09bff8def0c1953a752eee67a5fa11
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-07-16 11:00:23 +0700

    up

---
+++ dist/precache-manifest.ba691bcc31c174c395d98109da4590f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5da7bc6cea133289b5e",
+    "url": "/css/chunk-vendors.7b062300.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bf9c244930c4d81de069a35d409b70de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c41d460453cffb9544",
+    "url": "/js/about.25cfd599.js"
+  },
+  {
+    "revision": "d275bb53faee2ef996da",
+    "url": "/js/app.1a698a95.js"
+  },
+  {
+    "revision": "a5da7bc6cea133289b5e",
+    "url": "/js/chunk-vendors.b2330d93.js"
+  },
+  {
+    "revision": "de2440d62c4c2ab941d4540b609632b6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
